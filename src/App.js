20b2a2139535a48019df6d537edccae238aaebb0
mehmeteyupoglu-osf <mehmeteyupoglu2@gmail.com>
2022-04-23 02:10:31 +0300

    chore: initial setup continued, test scss files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Button from "./components/atoms/Button";
 import './App.css';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-        <Button />
+        <Home/>
     </div>
   );
 }
---
+++ src/components/molecules/ButtonGroup/index.js
@@ -0,0 +1,13 @@
+import Button from "../../atoms/Button";
+import "./ButtonGroup.scss";
+
+function ButtonGroup() {
+  return (
+    <div className="button-group">
+        <Button/>
+        <Button/>
+    </div>
+  );
+}
+
+export default ButtonGroup;
