e74e59b06e9033dea2ed8dcee551b04ee1786648
mehmeteyupoglu-osf <mehmeteyupoglu2@gmail.com>
2022-04-23 16:43:25 +0300

    fix: introduce navigate in products

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,13 @@
 import CustomButton from "../../components/atoms/Button";
 import "./Products.scss";
+import { useNavigate } from "react-router-dom";
 
 function Products() {
+    let navigate = useNavigate();
     return (
         <div className="products-page centered-pages">
             <h1>Products</h1>
-            <CustomButton title={'Go Back'} onClick={() => window.history.back()} />
+            <CustomButton title={'Go Back'} onClick={() => navigate(`/`)} />
         </div>
 
     );
